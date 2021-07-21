02048787c6d60f65f32470b97daad1ec0394354e
020697Dmv <55223897+020697Dmv@users.noreply.github.com>
2021-07-21 10:43:00 -0500

    Entity
    Entitys

--- src/main/java/com/crud/entity/Bet.java
+++ src/main/java/com/crud/entity/Bet.java
@@ -1,5 +1,53 @@
 package com.crud.entity;
 
-public class Bet {
+import org.springframework.data.redis.core.index.Indexed;
 
+/**
+ * Entity for roulette class 
+ * 
+ * @author Danny Macias Vanegas
+ *
+ */
+public class Bet {
+	@Indexed
+	private Long idBet;
+	private Long money;
+	private String idUser;
+	private String color;
+	private Long number;
+	public Long getIdBet() {
+		
+		return idBet;
+	}
+	public void setIdBet(Long idBet) {
+		this.idBet = idBet;
+	}
+	public Long getMoney() {
+		
+		return money;
+	}
+	public void setMoney(Long money) {
+		this.money = money;
+	}
+	public String getIdUser() {
+		
+		return idUser;
+	}
+	public void setIdUser(String idUser) {
+		this.idUser = idUser;
+	}
+	public String getColor() {
+		
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public Long getNumber() {
+		
+		return number;
+	}
+	public void setNumber(Long number) {
+		this.number = number;
+	}	
 }
--- src/main/java/com/crud/entity/Roulette.java
+++ src/main/java/com/crud/entity/Roulette.java
@@ -1,5 +1,52 @@
 package com.crud.entity;
 
-public class Roulette {
+import java.util.HashMap;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+/**
+ * Entity for roulette class 
+ * 	
+ * @author Danny Macias Vanegas
+ *
+ */
+@RedisHash("Roulette")
+public class Roulette {	
+	@Id
+	private Long idRoulette;
+	private HashMap<Integer, Bet> betList;
+	private String status;	
+	public Roulette(String status) {
+		this.status = status;
+	}
+	public Roulette() {
+		betList = new HashMap<>();
+	}
+	public Long getIdRoulette() {
+		
+		return idRoulette;
+	}
+	public void setIdRoulette(Long idRoulette) {		
+		this.idRoulette = idRoulette;
+	}
+	public HashMap<Integer, Bet> getBetList() {
+		
+		return betList;
+	}
+	public void setBetList(HashMap<Integer, Bet> betList) {
+		this.betList = betList;
+	}
+	public String getStatus() {
+		
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public void addBetToList(Bet bet) {
+		int key = betList.size();
+		betList.put(key, bet);
+	}
 }
+
